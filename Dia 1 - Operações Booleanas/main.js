898c47c08cc93d49bdd3c2e9fed51fe20e3f9a80
Luc <lucthemit@gmail.com>
2022-09-25 21:36:23 -0300

    Primeiro dia do desafio #7DaysOfCode da Alura

--- Dia 1 - Operações Booleanas/main.js
+++ Dia 1 - Operações Booleanas/main.js
@@ -10,32 +10,41 @@ let stringDez = '10';
 
 if (numeroUm == stringUm) {
     if (typeof numeroUm === typeof stringUm) {
-        console.log('As variáveis numeroUm e stringUm têm o mesmo valor e o mesmo tipo.');
+        document.querySelector('.numeroUm').innerHTML = `As variáveis numeroUm (${numeroUm}) e stringUm ("${stringUm}") têm o mesmo valor e o mesmo tipo.`;
+        console.log(`As variáveis numeroUm (${numeroUm}) e stringUm ("${stringUm}") têm o mesmo valor e o mesmo tipo.`);
     } else {
-        console.log('As variáveis numeroUm e stringUm têm o mesmo valor, mas tipos diferentes.');
+        document.querySelector('.numeroUm').innerHTML = `As variáveis numeroUm (${numeroUm}) e stringUm ("${stringUm}") têm o mesmo valor, mas tipos diferentes.`;
+        console.log(`As variáveis numeroUm (${numeroUm}) e stringUm ("${stringUm}") têm o mesmo valor, mas tipos diferentes.`);
     }
 } else {
-    console.log('As variáveis numeroUm e stringUm não têm o mesmo valor.');
-}
-
-
-if (numeroTrinta == stringTrinta) {
-    if (typeof numeroTrinta === typeof stringTrinta) {
-        console.log('As variáveis numeroUm e stringUm têm o mesmo valor e o mesmo tipo.');
-    } else {
-        console.log('As variáveis numeroUm e stringUm têm o mesmo valor, mas tipos diferentes.');
-    }
-} else {
-    console.log('As variáveis numeroUm e stringUm não têm o mesmo valor.');
+    document.querySelector('.numeroUm').innerHTML = `As variáveis numeroUm (${numeroUm}) e stringUm ("${stringUm}") não têm o mesmo valor.`;
+    console.log(`As variáveis numeroUm (${numeroUm}) e stringUm ("${stringUm}") não têm o mesmo valor.`);
 }
 
 
 if (numeroDez == stringDez) {
     if (typeof numeroDez === typeof stringDez) {
-        console.log('As variáveis numeroDez e stringDez têm o mesmo valor e o mesmo tipo.');
+        document.querySelector('.numeroDez').innerHTML = `As variáveis numeroDez (${numeroDez}) e stringDez ("${stringDez}") têm o mesmo valor e o mesmo tipo.`;
+        console.log(`As variáveis numeroDez (${numeroDez}) e stringDez ("${stringDez}") têm o mesmo valor e o mesmo tipo.`);
     } else {
-        console.log('As variáveis numeroUm e stringUm têm o mesmo valor, mas tipos diferentes.');
+        document.querySelector('.numeroDez').innerHTML = `As variáveis numeroDez (${numeroDez}) e stringDez ("${stringDez}") têm o mesmo valor, mas tipos diferentes.`;
+        console.log(`As variáveis numeroDez (${numeroDez}) e stringDez ("${stringDez}") têm o mesmo valor, mas tipos diferentes.`);
     }
 } else {
-    console.log('As variáveis numeroUm e stringUm não têm o mesmo valor.');
+    document.querySelector('.numeroDez').innerHTML = `As variáveis numeroDez (${numeroDez}) e stringDez ("${stringDez}") não têm o mesmo valor.`;
+    console.log(`As variáveis numeroDez (${numeroDez}) e stringDez ("${stringDez}") não têm o mesmo valor.`);
+}
+
+
+if (numeroTrinta == stringTrinta) {
+    if (typeof numeroTrinta === typeof stringTrinta) {
+        document.querySelector('.numeroTrinta').innerHTML = `As variáveis numeroTrinta (${numeroTrinta}) e stringTrinta ("${stringTrinta}") têm o mesmo valor e o mesmo tipo.`;
+        console.log(`As variáveis numeroTrinta (${numeroTrinta}) e stringTrinta ("${stringTrinta}") têm o mesmo valor e o mesmo tipo.`);
+    } else {
+        document.querySelector('.numeroTrinta').innerHTML = `As variáveis numeroTrinta (${numeroTrinta}) e stringTrinta ("${stringTrinta}") têm o mesmo valor, mas tipos diferentes.`;
+        console.log(`As variáveis numeroTrinta (${numeroTrinta}) e stringTrinta ("${stringTrinta}") têm o mesmo valor, mas tipos diferentes.`);
+    }
+} else {
+    document.querySelector('.numeroTrinta').innerHTML = `As variáveis numeroTrinta (${numeroTrinta}) e stringTrinta ("${stringTrinta}") não têm o mesmo valor.`;
+    console.log(`As variáveis numeroTrinta (${numeroTrinta}) e stringTrinta ("${stringTrinta}") não têm o mesmo valor.`);
 }
